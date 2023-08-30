5a37385d513b6acd2f235994e0d5ba5237f783bf
Ian Ricardo García Delgado <51796266+IanRicGD@users.noreply.github.com>
2023-08-30 15:19:55 -0600

    Update Actividad3.py

--- Dia3/Actividad3.py
+++ Dia3/Actividad3.py
@@ -7,20 +7,17 @@ a=[8,9,10,6,6,7,8,9,5,6,7,6,8,8,9,9,5,6,7,9,10,7,8,9,9]
 def promedio(arreglo):
     tmp=0
     prom=0
-    for i in a:
+    for i in arreglo:
         tmp+=i
     prom =tmp/len(arreglo)
     return prom
 
 def reprobaron(arreglo):
     reprobados=0
-    for i in a:
+    for i in arreglo:
         if(i<6):
             reprobados+=1
     return(reprobados)
 
 print("El promedio de las calificaciones es: "+str(promedio(a)))
 print("El número de reprobados es: "+str(reprobaron(a)))
-
-
-            
