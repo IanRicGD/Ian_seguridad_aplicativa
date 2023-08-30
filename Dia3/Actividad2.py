39da406f79051917fced09305e5a7b993e5255a6
Ian Ricardo García Delgado <51796266+IanRicGD@users.noreply.github.com>
2023-08-30 12:33:30 -0600

    Update Actividad2.py

--- Dia3/Actividad2.py
+++ Dia3/Actividad2.py
@@ -6,52 +6,61 @@
 
 menu="Calculadora \n  Opción 1: Suma \n  Opción 2: Resta \n  Opción 3: Multiplcación \n  Opción 4: División "
 
-i=True
+def suma(x,y):return x+y
+def resta(x,y):return x-y
+def mult(x,y):return x*y
+def div(x,y):return x/y
 
-while(i==True):
-    print(menu)
-    #Opción seleccionada
-    opcion = float(input("Ingresa una opción: "))
-    
-    if (opcion==1):
-        #Primer número
-        num1 = float(input ("Ingresa el primer número: "))
-        #Segundo numero
-        num2= float(input("Ingresa el segundo número: "))
-        print("El resultado de la suma es: " + str(num1+num2))
-    elif (opcion==2):
-        #Primer número
-        num1 = float(input ("Ingresa el primer número: "))
-        #Segundo numero
-        num2= float(input("Ingresa el segundo número: "))
-        print("El resultado de la resta es: " + str(num1-num2))
-    elif (opcion==3):
-        #Primer número
-        num1 = float(input ("Ingresa el primer número: "))
-        #Segundo numero
-        num2= float(input("Ingresa el segundo número: "))
-        print("El resultado de la multiplicación es: " + str(num1*num2))
-    elif (opcion==4):
-        #Primer número
-        num1 = float(input ("Ingresa el primer número: "))
-        #Segundo numero
-        num2= float(input("Ingresa el segundo número: "))
-        if(num2==0):
-            print("No es posible realizar una división entre 0")
+def calculadora():
+        print(menu)
+        #Opción seleccionada
+        opcion = float(input("Ingresa una opción: "))
+        
+        if (opcion==1):
+            #Primer número
+            num1 = float(input ("Ingresa el primer número: "))
+            #Segundo numero
+            num2= float(input("Ingresa el segundo número: "))
+            print("El resultado de la suma es: " + str(suma(num1,num2)))
+        elif (opcion==2):
+            #Primer número
+            num1 = float(input ("Ingresa el primer número: "))
+            #Segundo numero
+            num2= float(input("Ingresa el segundo número: "))
+            print("El resultado de la resta es: " + str(resta(num1,num2)))
+        elif (opcion==3):
+            #Primer número
+            num1 = float(input ("Ingresa el primer número: "))
+            #Segundo numero
+            num2= float(input("Ingresa el segundo número: "))
+            print("El resultado de la multiplicación es: " + str(mult(num1,num2)))
+        elif (opcion==4):
+            #Primer número
+            num1 = float(input ("Ingresa el primer número: "))
+            #Segundo numero
+            num2= float(input("Ingresa el segundo número: "))
+            if(num2==0):
+                print("No es posible realizar una división entre 0")
+            else:
+                print("El resultado de la división es: " + str(div(num1,num2)))
+        else: 
+            print("La opción es incorrecta")
+            
+        respuesta = int(input("¿Quieres realizar otra operación? Ingresa un 1 y para sí o 0 para no: "))
+        
+        if (respuesta==1):
+            return True
+        elif (respuesta==0):
+            return False
         else:
-            print("El resultado de la división es: " + str(num1/num2))
-    else: 
-        print("La opción es incorrecta")
-        
-    respuesta = int(input("¿Quieres realizar otra operación? Ingresa un 1 y para sí o 0 para no: "))
-    
-    if (respuesta==1):
-        i=True
-    elif (respuesta==0):
-        i=False
-    else:
-        print("Opción incorrecta")
-        i=False
+            print("Opción incorrecta")
+            return False
+            
+
         
 
+i=True
+while(i==True):
+    i=calculadora()
+    
 print("Hasta luego")
